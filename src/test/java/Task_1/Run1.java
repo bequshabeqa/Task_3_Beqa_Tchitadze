6b9596646d9dec29bed70b9d53d8e2536ab77292
Beka Chitadze <bchitadze@credo.ge>
2024-10-09 17:11:26 +0400

    get

--- src/test/java/Task_1/Run1.java
+++ src/test/java/Task_1/Run1.java
@@ -2,7 +2,8 @@ package Task_1;
 
 public class Run1 {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test3");
         System.out.println("Teset2");
+        System.out.println("test7");
     }
 }
