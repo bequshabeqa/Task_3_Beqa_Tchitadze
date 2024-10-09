07319e7b53806b1b01cc0033479e2e7850c164ac
Beka Chitadze <bchitadze@credo.ge>
2024-10-09 19:18:54 +0400

    Task_4 davamate

--- src/test/java/Task_4/Run4.java
+++ src/test/java/Task_4/Run4.java
@@ -1,4 +1,29 @@
 package Task_4;
 
-public class Run4 {
-}
+import java.util.Scanner;
+
+    class BasicCalculator {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            // Input Amount valute, Exchange rate, Received valute
+            System.out.print("Enter Amount valute: ");
+            double Amount = scanner.nextDouble();
+
+            System.out.print("Enter Exchange rate: ");
+            double Exchange = scanner.nextDouble();
+
+            System.out.print("Enter Fee Percentage: ");
+            double Fee = scanner.nextDouble();
+
+            // Calculate Exchange Rate
+            double EUR = Amount * Exchange * (1-(Fee/100));
+
+
+            // Print result
+            System.out.printf("EUR Received: %.2f\n", EUR);
+
+            // Close the scanner
+            scanner.close();
+        }
+    }
