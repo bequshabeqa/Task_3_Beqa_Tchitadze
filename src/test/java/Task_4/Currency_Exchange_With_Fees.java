c763bafe54915137a21c31d291c652b4119565c7
Beka Chitadze <bchitadze@credo.ge>
2024-10-10 14:59:25 +0400

    Task4_Tchitadze

--- src/test/java/Task_4/Currency_Exchange_With_Fees.java
+++ src/test/java/Task_4/Currency_Exchange_With_Fees.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
         public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input Amount valute, Exchange rate, Received valute
-        System.out.print("Enter Amount valute: ");
+        // ჩაწერე გადასახდელი ვალუტა, გაცვლის კურსი, მისაღები ვალუტა
+        System.out.print("ჩაწერე გადასახდელი ვალუტა: ");
         double Amount = scanner.nextDouble();
 
-        System.out.print("Enter Exchange rate: ");
+        System.out.print("ჩაწერე გაცვლითი კურსი: ");
         double Exchange = scanner.nextDouble();
 
-        System.out.print("Enter Fee Percentage: ");
+        System.out.print("ჩაწერე მისაღები ვალუტა: ");
         double Fee = scanner.nextDouble();
 
-        // Calculate Exchange Rate
+        // გაცვლითი კალკულატორი
         double EUR = Amount * Exchange * (1-(Fee/100));
 
 
-        // Print result
+        // დაბეჭდე შედეგი
         System.out.printf("EUR Received: %.2f\n", EUR);
 
         // Close the scanner
